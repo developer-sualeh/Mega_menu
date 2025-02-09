117d77b72a410c774db27414387634842fb1c6d6
developer-sualeh <imsualeh@gmail.com>
2025-02-09 23:46:14 +0530

    improve code

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const angleDownEl = document.querySelector(".fa-angle-down");
 const navEl = document.querySelector(".navbar-container");
 const dropDownLinkEl = document.getElementById("drop-down--link");
 const megaDropDownEl = document.querySelector(".mega-drop--down");
+const megaDropDownSubLinkEl = document.querySelector(
+  ".mega-dropdown-sub--link"
+);
+const megaMenuSubDropDownSubLinkEl = document.querySelectorAll(".question");
 const navContainerEl = document.querySelector(".navbar-container");
 
 barEl.addEventListener("click", () => {
@@ -14,4 +18,14 @@ dropDownLinkEl.addEventListener("click", () => {
   megaDropDownEl.classList.toggle("show");
   angleDownEl.classList.toggle("rotate");
   navContainerEl.style.height = "auto";
+});
+
+megaMenuSubDropDownSubLinkEl.forEach((link) => {
+  console.log(link);
+  const btn = link.querySelector(".question-btn");
+  console.log(btn);
+  btn.addEventListener("click", () => {
+    console.log("click");
+    link.classList.toggle("show-text");
+  });
 });
